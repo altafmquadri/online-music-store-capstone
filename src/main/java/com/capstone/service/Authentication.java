8b25e9fdb5ee7a63b59371085341bc38574b0baf
Altaf Quadri <altafmquadri@gmail.com>
2021-01-24 00:28:14 -0500

    changes to Authentication class

--- src/main/java/com/capstone/service/Authentication.java
+++ src/main/java/com/capstone/service/Authentication.java
@@ -8,6 +8,7 @@ import com.capstone.dao.CustomerDAO;
 public class Authentication<T> implements AuthenticationInterface{
 
 	private T currentUser = null;
+	
 	@Autowired
 	AdminDAO adminDao;
 	
@@ -16,7 +17,11 @@ public class Authentication<T> implements AuthenticationInterface{
 	
 	@Override
 	public boolean authenticate(String userName, String password) {
+		//if I pass a customer it should use customerDao 
 		
+		//if I pass an admin it should use the adminDao
+		
+		//some method implementation
 		
 		return false;
 	}
@@ -25,6 +30,14 @@ public class Authentication<T> implements AuthenticationInterface{
 	public void logout() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public T getCurrentUser() {
+		return currentUser;
+	}
+
+	public void setCurrentUser(T currentUser) {
+		this.currentUser = currentUser;
 	}
 
 
