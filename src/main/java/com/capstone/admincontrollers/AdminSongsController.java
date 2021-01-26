747123932b42610da71d6824902690d66d16b545
neilmukherjee96 <neilmukherjee96@gmail.com>
2021-01-26 12:49:55 -0500

    post method and cancel for the edit function

--- src/main/java/com/capstone/admincontrollers/AdminSongsController.java
+++ src/main/java/com/capstone/admincontrollers/AdminSongsController.java
@@ -44,11 +44,26 @@ public class AdminSongsController {
 	}
 
 	@GetMapping("admin/songs/editsong/{id}")
-	public ModelAndView editSong(@PathVariable("id") int id) {
+	public ModelAndView showEdit(@PathVariable("id") int id) {
 		Song song = songDao.findById(id).get();
 		return new ModelAndView("editsong").addObject(song);
 	}
-	
 
+	@PostMapping("admin/songs/editsong/{id}")
+	public ModelAndView editSong(@PathVariable("id") int id, @RequestParam("title") String title,
+			@RequestParam("description") String description, @RequestParam("artist") String artist,
+			@RequestParam("genre") String genre, @RequestParam("format") String format,
+			@RequestParam("price") double price) {
+
+		Song song = songDao.findById(id).get();
+		song.setTitle(title);
+		song.setDescription(description);
+		song.setArtist(artist);
+		song.setGenre(genre);
+		song.setFormat(format);
+		song.setPrice(price);
+		songDao.save(song);
+		return new ModelAndView("redirect:/admin/songs");
+	}
 
 }
